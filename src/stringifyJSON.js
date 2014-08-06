db084e53e620e874b311e11d4b20f28383e77867
Kevin Meurer <kevin.a.meurer@gmail.com>
2014-08-05 23:14:07 -0600

    Finished stringifyJSON!  Done with the basic requirements for recursion now.  Will look for opportunities to refactor stringifyJSON next.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,37 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+    if (typeof obj == "number" || obj === null || typeof obj == "boolean" || obj === undefined){
+      return String(obj);
+    }
+    else if (typeof obj == "string"){
+      return '"' + obj + '"';
+    }
+    else if (typeof obj == "object"){
+        var result = [];
+        if (Array.isArray(obj)){
+            for(var i = 0; i < obj.length; i++){
+                result.push(stringifyJSON(obj[i]));
+            }
+            return "[" + result.join(",") + "]";
+        }
+        else {
+        var keys = Object.keys(obj);
+        for (var i =0; i < keys.length; i++){
+          var addedString = '"' + keys[i] + '":';
+          var keyVal = obj[keys[i]];
+          if (typeof keyVal == "function" || keyVal === undefined){
+            //do nothing
+          }
+          else{
+            addedString = addedString + stringifyJSON(obj[keys[i]]);
+          result.push(addedString);
+            }
+        }
+          return "{" + result.join(",") + "}";
+        }
+    }
+    else {
+        return obj;
+    }
 };
